27957fd6e81566ab789c471be16356fe2dbc1372
FIRST_OSCAR LAST_JNR <immanuelsowahoscar@gmail.com>
2023-01-01 13:54:58 +0000

    Mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'Logins.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: Logins(),
     );
   }
 }
