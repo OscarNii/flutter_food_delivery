914be8a6ef8c56116b1d6fd3cae6520194f76b25
FIRST_OSCAR LAST_JNR <immanuelsowahoscar@gmail.com>
2023-01-05 10:36:33 +0000

    navigations

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: sized_box_for_whitespace, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Homepage1 extends StatelessWidget {
+  const Homepage1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text("Main"),
+      centerTitle: true,
+    ));
+  }
+}
--- lib/signup.dart
+++ lib/signup.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
-import 'package:flutter_food_delivery/homepage.dart';
+import 'package:flutter_food_delivery/pages/homepage.dart';
 import 'package:flutter_food_delivery/pages/page1.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -12,101 +12,77 @@ class Signup extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: Container(
-        child: Stack(
-          children: [
-            Page1(),
-            SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 320.0),
-                child: Container(
-                  width: 70,
-                  height: 30,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 174, 244, 54),
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: ((context) => Homepage1())));
-                    },
-                    child: Text(
-                      'skip',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                          color: Colors.black),
-                    ),
+      body: Stack(
+        children: [
+          Page1(),
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            body: SafeArea(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 120.0),
+                  child: Column(
+                    children: [
+                      Center(
+                        child: Text(
+                          'WELCOME',
+                          style: GoogleFonts.acme(
+                            textStyle: TextStyle(
+                              color: Colors.white,
+                              fontSize: 75,
+                              shadows: <Shadow>[
+                                Shadow(
+                                    offset: Offset(10.0, 10.0),
+                                    blurRadius: 30.0,
+                                    color: Colors.white30),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Divider(),
+                      Text(
+                        'Your favourite foods delivered',
+                        style: TextStyle(
+                          fontSize: 19,
+                          color: Colors.white,
+                          shadows: <Shadow>[
+                            Shadow(
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 3.0,
+                              color: Colors.black87,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Divider(),
+                      Text(
+                        'Fast at your door step',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          shadows: <Shadow>[
+                            Shadow(
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 3.0,
+                              color: Colors.black87,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Divider(),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                            onPressed: () {}, child: Text("FOOD")),
+                      )
+                    ],
                   ),
                 ),
               ),
             ),
-            Scaffold(
-              backgroundColor: Colors.transparent,
-              body: SafeArea(
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 120.0),
-                    child: Column(
-                      children: [
-                        Center(
-                          child: Text(
-                            'WELCOME',
-                            style: GoogleFonts.acme(
-                              textStyle: TextStyle(
-                                color: Colors.white,
-                                fontSize: 75,
-                                shadows: <Shadow>[
-                                  Shadow(
-                                      offset: Offset(10.0, 10.0),
-                                      blurRadius: 30.0,
-                                      color: Colors.white30),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        Divider(),
-                        Text(
-                          'Your favourite foods delivered',
-                          style: TextStyle(
-                            fontSize: 19,
-                            color: Colors.white,
-                            shadows: <Shadow>[
-                              Shadow(
-                                offset: Offset(2.0, 2.0),
-                                blurRadius: 3.0,
-                                color: Colors.black87,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Divider(),
-                        Text(
-                          'Fast at your door step',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.white,
-                            shadows: <Shadow>[
-                              Shadow(
-                                offset: Offset(2.0, 2.0),
-                                blurRadius: 3.0,
-                                color: Colors.black87,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
