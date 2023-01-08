0508dc87ebe51d3e39404759a4e6393dbe14b19f
FIRST_OSCAR LAST_JNR <immanuelsowahoscar@gmail.com>
2023-01-08 18:19:38 +0000

    HomePage Update

--- lib/pages/allsign.dart
+++ lib/pages/allsign.dart
@@ -3,6 +3,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 
+import 'homepage.dart';
+
 void main() => runApp(MyApp1());
 
 class MyApp1 extends StatelessWidget {
@@ -11,7 +13,7 @@ class MyApp1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sign',
       theme: ThemeData.light(),
       home: Scaffold(
         backgroundColor: Colors.transparent,
@@ -28,7 +30,7 @@ class SignInPage extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text('$provider Button Pressed!'),
-        backgroundColor: Colors.black26,
+        backgroundColor: Colors.transparent,
         duration: const Duration(milliseconds: 400),
       ),
     );
@@ -37,99 +39,113 @@ class SignInPage extends StatelessWidget {
   /// Normally the signin buttons should be contained in the SignInPage
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 200.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SignInButtonBuilder(
-              highlightColor: Colors.red,
-              text: 'Get going with Email',
-              icon: Icons.email,
+    return Column(
+      children: [
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 330.0),
+            child: ElevatedButton(
               onPressed: () {
-                _showButtonPressDialog(context, 'Email');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Homepage1(),
+                  ),
+                );
               },
-              backgroundColor: const Color.fromARGB(255, 154, 235, 56),
-              width: 220.0,
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                fixedSize: Size(80, 40),
+                textStyle: TextStyle(fontSize: 18),
+                primary: Color.fromARGB(255, 154, 235, 56),
+                onPrimary: Color.fromARGB(255, 0, 0, 0),
+                shadowColor: Color.fromARGB(255, 255, 255, 255),
+                side: BorderSide(
+                    color: Color.fromARGB(255, 255, 255, 255), width: 2),
+                shape: StadiumBorder(),
+              ),
+              child: Text("SKIP"),
             ),
-            const Divider(),
-            Container(
-              child: SignInButton(
-                Buttons.GoogleDark,
+          ),
+        ),
+        Divider(),
+        Padding(
+          padding: const EdgeInsets.only(top: 300.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SignInButtonBuilder(
+                text: 'Get going with Email',
+                icon: Icons.email,
                 onPressed: () {
-                  _showButtonPressDialog(context, 'Google (dark)');
+                  _showButtonPressDialog(context, 'Email');
+                },
+                backgroundColor: Colors.blueGrey[700]!,
+                width: 220.0,
+              ),
+              const Divider(),
+              SignInButton(
+                Buttons.Apple,
+                onPressed: () {
+                  _showButtonPressDialog(context, 'Apple');
                 },
               ),
-              width: 380,
-              margin: EdgeInsets.all(7),
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Color.fromARGB(255, 41, 106, 217)),
-            ),
-            const Divider(),
-            SignInButton(
-              Buttons.Apple,
-              onPressed: () {
-                _showButtonPressDialog(context, 'Apple');
-              },
-            ),
-            const Divider(),
-            SignInButton(
-              Buttons.GitHub,
-              text: 'Sign up with GitHub',
-              onPressed: () {
-                _showButtonPressDialog(context, 'Github');
-              },
-            ),
-            const Divider(),
-            SignInButton(
-              Buttons.Twitter,
-              text: 'Use Twitter',
-              onPressed: () {
-                _showButtonPressDialog(context, 'Twitter');
-              },
-            ),
-            const Divider(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SignInButton(
-                  Buttons.LinkedIn,
-                  mini: true,
-                  onPressed: () {
-                    _showButtonPressDialog(context, 'LinkedIn (mini)');
-                  },
-                ),
-                SignInButton(
-                  Buttons.Tumblr,
-                  mini: true,
-                  onPressed: () {
-                    _showButtonPressDialog(context, 'Tumblr (mini)');
-                  },
-                ),
-                SignInButton(
-                  Buttons.Facebook,
-                  mini: true,
-                  onPressed: () {
-                    _showButtonPressDialog(context, 'Facebook (mini)');
-                  },
-                ),
-                SignInButtonBuilder(
-                  icon: Icons.email,
-                  text: 'Ignored for mini button',
-                  mini: true,
-                  onPressed: () {
-                    _showButtonPressDialog(context, 'Email (mini)');
-                  },
-                  backgroundColor: Colors.cyan,
-                ),
-              ],
-            ),
-          ],
+              const Divider(),
+              SignInButton(
+                Buttons.GitHub,
+                text: 'Sign up with GitHub',
+                onPressed: () {
+                  _showButtonPressDialog(context, 'Github');
+                },
+              ),
+              const Divider(),
+              SignInButton(
+                Buttons.Twitter,
+                text: 'Use Twitter',
+                onPressed: () {
+                  _showButtonPressDialog(context, 'Twitter');
+                },
+              ),
+              const Divider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SignInButton(
+                    Buttons.LinkedIn,
+                    mini: true,
+                    onPressed: () {
+                      _showButtonPressDialog(context, 'LinkedIn (mini)');
+                    },
+                  ),
+                  SignInButton(
+                    Buttons.Tumblr,
+                    mini: true,
+                    onPressed: () {
+                      _showButtonPressDialog(context, 'Tumblr (mini)');
+                    },
+                  ),
+                  SignInButton(
+                    Buttons.Facebook,
+                    mini: true,
+                    onPressed: () {
+                      _showButtonPressDialog(context, 'Facebook (mini)');
+                    },
+                  ),
+                  SignInButtonBuilder(
+                    icon: Icons.email,
+                    text: 'Ignored for mini button',
+                    mini: true,
+                    onPressed: () {
+                      _showButtonPressDialog(context, 'Email (mini)');
+                    },
+                    backgroundColor: Colors.cyan,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
--- lib/signup.dart
+++ lib/signup.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery/pages/allsign.dart';
+import 'package:flutter_food_delivery/pages/homepage.dart';
 import 'package:flutter_food_delivery/pages/page1.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -17,27 +18,9 @@ class Signup extends StatelessWidget {
           SafeArea(
             child: Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 330.0),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(horizontal: 10.0),
-                      fixedSize: Size(80, 40),
-                      textStyle: TextStyle(fontSize: 18),
-                      primary: Color.fromARGB(255, 154, 235, 56),
-                      onPrimary: Color.fromARGB(255, 0, 0, 0),
-                      shadowColor: Color.fromARGB(255, 255, 255, 255),
-                      side: BorderSide(
-                          color: Color.fromARGB(255, 255, 255, 255), width: 2),
-                      shape: StadiumBorder(),
-                    ),
-                    child: Text("SKIP"),
-                  ),
-                ),
                 Divider(),
                 Padding(
-                  padding: const EdgeInsets.only(top: 28.0),
+                  padding: const EdgeInsets.only(top: 45.0),
                   child: Text(
                     "Welcome",
                     style: GoogleFonts.acme(
