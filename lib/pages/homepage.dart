f7e28e251a94eceb2556e5298aad77f2db514146
FIRST_OSCAR LAST_JNR <immanuelsowahoscar@gmail.com>
2023-01-10 16:17:10 +0000

    elevated buttom

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: sized_box_for_whitespace, prefer_const_constructors
+// ignore_for_file: sized_box_for_whitespace, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -14,35 +14,53 @@ class Homepage1 extends StatelessWidget {
           SafeArea(
             child: Padding(
               padding: const EdgeInsets.only(bottom: 20.0),
-              child: SizedBox(
+              child: Container(
                 width: 430,
                 height: 200,
-                child: Image.asset(
-                  "asset/mcdonaldsglobal-2.jpg",
-                  fit: BoxFit.cover,
-                ),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    image: DecorationImage(
+                      image: AssetImage("asset/mcdonaldsglobal-2.jpg"),
+                      fit: BoxFit.cover,
+                    )),
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 60.0, left: 10),
-            child: Container(
-              width: 40,
-              height: 40,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 10.0),
-                child: Icon(
-                  Icons.arrow_back_ios,
-                  color: Colors.black,
-                  size: 30,
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0, left: 10),
+              child: Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.black,
+                    size: 30,
+                  ),
                 ),
               ),
             ),
-          )
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0, right: 10),
+              child: Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+          ])
         ],
       ),
     );
